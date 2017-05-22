81b9d67b7a276bc6a54d2008378dbc10ed563e26
Quin Kennedy <quinkennedy@gmail.com>
2017-05-22 16:32:53 -0500

    updates for oF 0.9

--- src/ofxSlitScan.cpp
+++ src/ofxSlitScan.cpp
@@ -181,7 +181,7 @@ void ofxSlitScan::setDelayMap(float* mappix){
 }
 
 void ofxSlitScan::setDelayMap(ofBaseHasPixels& map){
-    setDelayMap(map.getPixelsRef());
+    setDelayMap(map.getPixels());
 }
 
 void ofxSlitScan::setDelayMap(ofPixels& map){
@@ -213,7 +213,7 @@ void ofxSlitScan::addImage(unsigned char* image){
 	outputIsDirty = true;	
 }
 void ofxSlitScan::addImage(ofBaseHasPixels& image){
-    addImage(image.getPixelsRef());
+    addImage(image.getPixels());
 }
 
 void ofxSlitScan::addImage(ofPixels& image){
@@ -227,7 +227,7 @@ void ofxSlitScan::addImage(ofPixels& image){
 ofImage& ofxSlitScan::getOutputImage(){
 	if(outputIsDirty){
 		//calculate the new distorted image
-		unsigned char* writebuffer = outputImage.getPixels();
+		unsigned char* writebuffer = outputImage.getPixels().getData();
 		unsigned char* outbuffer = writebuffer;
 		
 		int x, y, offset, lower_offset, upper_offset, pixelIndex;
@@ -285,7 +285,7 @@ ofImage& ofxSlitScan::getOutputImage(){
 
 ofImage& ofxSlitScan::getDelayMap(){
 	if(delayMapIsDirty){
-		unsigned char* pix = delayMapImage.getPixels();
+		unsigned char* pix = delayMapImage.getPixels().getData();
 		for(int i = 0; i < width*height; i++){
 			pix[i] = char(delayMapPixels[i]*255);
 		}
